1731d98b321bbce43c9cfa81d6a904014395f698
zip95297 <zip95297@gmail.com>
2024-05-03 19:19:02 +0800

    添加剪枝后的模型

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,3 @@
 from .resnet import ResIRSE
 from .resnet18 import ResNet18
+from .resnet18_with_cfg import ResNet18_with_config
--- models/net_summary.py
+++ models/net_summary.py
@@ -1,11 +1,14 @@
 from torchsummary import summary as smy
 from resnet18 import ResNet18
 from resnet import ResIRSE
+from resnet18_with_cfg import ResNet18_with_config
 import torch
 from thop import profile
 
-#model = ResNet18()
 model = ResIRSE(embedding_size=512,drop_ratio=0.5)
+model = ResNet18()
+model = ResNet18_with_config(config= [512, 60, 'M', 64, 64, 64, 63, 128, 128, 53, 128, 128, 256, 256, 8, 247, 222, 497, 377, 5, 420, 510])
+
 smy(model, input_size=( 1, 128, 128), batch_size=64)
 
 input = torch.randn(1, 1, 128, 128)
